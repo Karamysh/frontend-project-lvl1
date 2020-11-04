f8279008051417608b710a35d40fdc8e6825706b
Dezenu <dezenu@gmail.com>
2020-11-04 15:36:20 +0300

    update

---
+++ brain-even.js
@@ -0,0 +1,17 @@
+import getRandom from './getRand.js';
+import launchGame from './index.js';
+
+const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (n) => n % 2 === 0;
+
+const getGameData = () => {
+  const question = getRandom();
+
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [correctAnswer, String(question)];
+};
+
+const launchGameEven = () => launchGame(getGameData, task);
+
+export default launchGameEven;
