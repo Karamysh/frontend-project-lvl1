77bcaaf4ac63fbe21309d24d6504dd3e6b493e19
Bitvinus <dezenu@ya.ru>
2020-11-28 16:35:08 +0300

    length move

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -17,9 +17,7 @@ const PROG_VALUES = {
   },
 };
 
-const PROGRESSION_LENGTH = getRandomNumber(PROG_VALUES.LENGTH.MIN, PROG_VALUES.LENGTH.MAX);
-
-const getProgression = (progStartAt, progStep) => {
+const getProgression = (progStartAt, progStep, PROGRESSION_LENGTH) => {
   const result = [];
   for (let i = 1; i <= PROGRESSION_LENGTH; i += 1) {
     result.push(progStartAt + (progStep * i));
@@ -28,9 +26,10 @@ const getProgression = (progStartAt, progStep) => {
 };
 
 const getGameData = () => {
+  const PROGRESSION_LENGTH = getRandomNumber(PROG_VALUES.LENGTH.MIN, PROG_VALUES.LENGTH.MAX);
   const progStep = getRandomNumber(PROG_VALUES.STEP.MIN, PROG_VALUES.STEP.MAX);
   const progStartAt = getRandomNumber(PROG_VALUES.START.MIN, PROG_VALUES.START.MAX);
-  const progression = getProgression(progStartAt, progStep);
+  const progression = getProgression(progStartAt, progStep, PROGRESSION_LENGTH);
   const missingIndex = getRandomNumber(0, PROGRESSION_LENGTH);
   const correctAnswer = progression[missingIndex];
   progression[missingIndex] = '..';
